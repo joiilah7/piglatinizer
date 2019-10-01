f36c70726ef329380f82877410a58fb68d7c2cfa
joiilah7 <joiilahh@gmail.com>
2019-10-01 01:28:39 +0000

    insert comment

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $( "document" ).ready(function() {
 
+//take the word out of the textbox and display it on the screen
+$("#button").click(function(){
+    let input = $(".input").val();
+    input.forEach(element => {
+
+    });
+
+
+ }
+
+
 
 
 });
